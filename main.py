88da0597734a7861a60f24e98b8b9f4de0fc6219
guanzhang lao <rhzzzzzz6@gmail.com>
2023-02-08 04:04:41 +0800

    with val_2

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def train_surrogate_model(model, data_root, seed):
 
 if __name__ == "__main__":
     model = XGBModel
-    data_root = 'data\\dataset'
+    data_root = './data/dataset'
     # model_config_path = 'config\\xgb_configspace.json'
     seed = 6
     train_surrogate_model(model, data_root, seed)
